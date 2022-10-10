302fbba141cd8423942b6c2ef92e83299971a5e7
NotNanetteKaye <nanettekayedolera@gmail.com>
2022-10-10 17:17:30 -0500

    Established Array of Objects in Apps.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+const App = () => {
+  const [dinos, setDinos] = useState([
+    {
+      id: 1,
+      name: 'Nanette-saurus',
+      type: 'Herbivore',
+      size: 'Extra Large',
+
+    },
+    {
+      id: 2,
+      name: 'Nanette-saurus',
+      type: 'Carnivore',
+      size: 'Extra Large',
+
+    },
+    {
+      id: 3,
+      name: 'Nanette-saurus',
+      type: 'Omnivore',
+      size: 'Extra Large',
+
+    },
+    {
+      id: 4,
+      name: 'Nanette-saurus',
+      type: 'Herbivore',
+      size: 'Extra Large',
+
+    },
+    {
+      id: 5,
+      name: 'Nanette-saurus',
+      type: 'Herbivore',
+      size: 'Extra Large',
+
+    },
+  ])
 }
-
+ 
 export default App;
+
