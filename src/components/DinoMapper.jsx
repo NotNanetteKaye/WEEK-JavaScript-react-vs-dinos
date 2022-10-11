0244368a788d50e64fa3c2b475c273a0e1e19cb9
NotNanetteKaye <nanettekayedolera@gmail.com>
2022-10-10 19:57:33 -0500

    Created Index.css To Begin Styling

--- src/components/DinoMapper.jsx
+++ src/components/DinoMapper.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import DinoPresenter from './DinoPresenter';
+import React from "react";
+import DinoPresenter from "./DinoPresenter";
 
+const DinoMapper = ({ dinoArray }) => {
+  return (
+    <ul>
+      {dinoArray.map((el) => (
+        <li>
+          <DinoPresenter key={el.id} dino={el} />
+        </li>
+      ))}
+    </ul>
+  );
+};
 
-const DinoMapper = ({dinoArray}) => {
-    return ( 
-        <ul>
-           {dinoArray.map(el => <li><DinoPresenter dino={el}/></li>)}
-        </ul>
-     );
-}
- 
 export default DinoMapper;
--- src/components/DinoPresenter.jsx
+++ src/components/DinoPresenter.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 const DinoPresenter = ({dino}) => {
     return ( 
+        <li>
         <div>
             <h2>{dino.name}</h2>
             <p>{dino.type}</p>
             <p>{dino.size}</p>
         </div>
+        </li>
      );
 }
  
